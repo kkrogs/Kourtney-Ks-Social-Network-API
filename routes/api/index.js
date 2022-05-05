19ce3a2cdb044c6b8ddeecfd53c581f0fe3980b5
kkrogs <kourtney.krogman@gmail.com>
2022-05-04 18:37:23 -0600

    more debugging

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
-// determining which route the user is going down
+// determines which route the user is going down
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
 
