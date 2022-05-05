7b9c3ff87ce158b402760c4f75a5eaa87d948d15
kkrogs <kourtney.krogman@gmail.com>
2022-05-04 18:36:50 -0600

    index.js adjustments

--- models/index.js
+++ models/index.js
@@ -2,5 +2,5 @@ const User = require('./User');
 const Thought = require('./Thought');
 const Reaction = require('./Reaction');
 
-// exporting all of our models
+// exporting all of the models
 module.exports = { User, Thought, Reaction };
